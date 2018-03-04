dea7b73f4c9dc8fa834f304f095055303fe47a3f
Nikolay Ponomarev <nikolay.a.ponomarev@gmail.com>
2018-03-03 17:18:16 -0800

    Removed import

--- test/src/main/java/configuration/ClientsConfig.java
+++ test/src/main/java/configuration/ClientsConfig.java
@@ -3,11 +3,9 @@ package configuration;
 import clients.UserClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestOperations;
 
 @Configuration
-@Import(ApplicationProperties.class)
 public class ClientsConfig {
 
     @Bean
